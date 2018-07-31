eade5100bcb4e9f12ace952e14c1fcc9c7b23e6a
Ronan Kelly <30551527+rmiguelkelly@users.noreply.github.com>
2018-07-31 13:59:58 -0400

    New Upload

---
+++ WinWeb/WinWeb/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            WinServer server = new WinServer();
+            server.RunServer(8080, callback);
+        }
+        static void callback(HttpRequest req, HttpResponse res)
+        {
+            Console.WriteLine(req.ToString());
+        }
+    }
+}
